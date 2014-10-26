ca92f3882cd0d32c5655cd578d8e299d43513732
Yoshie <yoshiemuranaka@gmail.com>
2014-10-26 01:40:29 -0400

    trying to declare global variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
 	def create
 
 		if params[:password] == params[:password_confirm]
+
+			if params[:handle].include?('@')
+				params[:handle].slice!(0)
+			end
+
 			user = User.create(
 				fname: params[:fname],
 				lname: params[:lname],
@@ -11,7 +16,41 @@ class UsersController < ApplicationController
 				password: params[:password]
 				)
 
-			# AJAX TWITTER API AND STORE TWEETS NOW --filter logic
+			client = $CLIENT
+			twitterAPI = client.user_timeline(user.handle, :count => 20)
+
+			twitterAPI.each do |tweet|
+				
+				value = 0
+				tweet.text.split('').each do |char|
+					if char == '#' || char == '@'
+						value += 3
+					end
+				end
+
+				if text.include?('http')
+					value +=5
+				end
+
+				time = Time.new(text.created_at)
+
+				if time.hour == 0 && time.hour < 8
+					color_code = 0
+				elsif time.hour == 8 && time.hour < 16
+					color_code = 1
+				else
+					color_code = 3
+				end
+
+				Tweet.create(
+					user_id: user.id,
+					tweet_created_at: tweet.created_at,
+					text: tweet.text,
+					value: value, 
+					color_code: color_code
+					)
+			
+			end
 
 			redirect_to '/'
 		else
--- db/migrate/20141026015408_add_password_column_to_user.rb
+++
@@ -1,4 +0,0 @@
-class AddPasswordColumnToUser < ActiveRecord::Migration
-  def change
-  end
-end
