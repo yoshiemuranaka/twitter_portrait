2421fb8ff6118617727c2b5a066966d327dd09ab
Yoshie <yoshiemuranaka@gmail.com>
2014-10-29 13:06:09 -0400

    removing clearinterval;

--- app/assets/javascripts/appD3.js
+++ app/assets/javascripts/appD3.js
@@ -1,8 +1,5 @@
 $(function() {
 	
-	clearInterval(timer)
-	// console.log('timer cleared user')
-
 	var size = 500
 
 	var color = ["#000033", "#2B1A3C", "#553344", "#804D4D", "#AA6655", "#D5805E", "#FF9966", "#FFA255", "#FFAA44", "#FFB333", "#FFBB22", "#FFC411", "#FFCC00", "#EEBB11", "#DDAA22", "#CC9933", "#BB8844", "#AA7755", "#996666", "#80555E", "#664455", "#4D334D", "#332244", "#1A113C"]
