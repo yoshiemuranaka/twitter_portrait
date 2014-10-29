e68445ed1e9e22186125bd811a99065abf844f73
Yoshie <yoshiemuranaka@gmail.com>
2014-10-29 10:36:25 -0400

    debug timer

--- app/assets/javascripts/appD3.js
+++ app/assets/javascripts/appD3.js
@@ -69,11 +69,6 @@ $(function() {
 		return circle
 	}
 
-	var timer = setInterval(function() {
-		circle = shuffleSelectTweet()
-		circle.__onclick()
-	}, 3000);
-
 	d3.select(self.frameElement).style('height', size)
 
 });
